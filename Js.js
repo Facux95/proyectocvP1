4ad916a6f02b1c3722904c94be3c0dc92289204f
Facundo Varas <facuux_95_22@hotmail.com>
2023-02-21 23:29:57 -0300

    sin links

---
+++ Js.js
@@ -0,0 +1,29 @@
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))//
+
+const boton = document.querySelector("#boton");
+const foto = document.getElementById("foto");
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const telefono = document.getElementById("telefono");
+
+console.log (boton);
+
+const generarUsuario = async() => {
+    try {
+        const url = "https://randomuser.me/api/";
+        const respuesta = await fetch (url);
+        const {results} = await respuesta.json();
+        const datos = results [0];
+        console.log (datos);
+        foto.src = datos.picture.large;
+        nombre.textContent = datos.name.first;
+        correo.textContent = datos.email;
+        telefono.textContent= datos.phone;
+    } catch (error) {
+        console.log (error); 
+    }
+}
+
+// boton.addEventListener("click", generarUsuario);
+document.addEventListener("DOMContentLoaded", generarUsuario);
